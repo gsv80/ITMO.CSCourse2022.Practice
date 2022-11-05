c8bc80e052144c73503c1135184c50eaf21ea5e0
Sergey Gorbanev <gsv80@ya.com>
2022-11-05 11:25:40 +0300

    Add project files.

---
+++ task1/ITMO.CSCourse2022.Labs.Lab1.Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab1.Task1{
+    internal class Greeting
+    {
+        static void Main()
+        {
+            string myName;
+            Console.WriteLine("Enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Welcome {0}", myName);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ task1/ITMO.CSCourse2022.Labs.Lab1.Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+namespace Lab1.Task4
+{
+    internal class ExceptionHandler
+    {
+        static void Main()
+        {
+            try
+            {
+                int i,j;
+                string tmp;
+                Console.WriteLine("Enter first number");
+                tmp = Console.ReadLine();
+                i = Int32.Parse(tmp);
+
+                Console.WriteLine("Enter second number");
+                tmp = Console.ReadLine();
+                j = Int32.Parse(tmp);
+
+                int k = i / j;
+                Console.WriteLine("The result of dividing {0} by {1} is {2}", i,j,k);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e.Message);
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
