ae284b027ac5a9d1b4596bd87d3708759525e5ba
Sergey Gorbanev <gsv80@ya.com>
2022-11-05 20:57:31 +0300

    Tasks 2 added

---
+++ task2/ITMO.CSCourse2022.Task2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ITMO.CSCourse2022.Task2._1
+{
+
+    public enum AccountType 
+    {  
+        Checking, 
+        Deposit 
+    }
+  
+    internal class ProgramEnum
+    {
+        static void Main()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+
+            Console.WriteLine("The Customer Account is: {0}", goldAccount);
+            Console.WriteLine("The Customer Account is: {0}", platinumAccount);
+        }
+    }
+}
---
+++ task2/ITMO.CSCourse2022.Task2.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ITMO.CSCourse2022.Task2._2
+{
+
+    public enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+    internal class ProgramStruct
+    {
+        static void Main()
+        {
+            BankAccount goldAccount;
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+            goldAccount.accNo = 123;
+
+            Console.WriteLine("*** Account Summary ***");
+            Console.WriteLine("Account Number: {0}", goldAccount.accNo);
+            Console.WriteLine("Account Type: {0}", goldAccount.accType);
+            Console.WriteLine("Account Balance: {0}", goldAccount.accBal);
+
+        }
+    }
+}
