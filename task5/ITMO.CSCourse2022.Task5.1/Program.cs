6082ad4c14637e1024d53d88626b84e02daa5497
Sergey Gorbanev <gsv80@ya.com>
2022-11-12 19:33:57 +0300

    temp

---
+++ task5/ITMO.CSCourse2022.Task5.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Task5._1
+{
+    internal class ProgramFileDetails
+    {
+        static void Main(string[] args)
+        {
+            string fileName = args[0];
+            FileStream stream = new FileStream(fileName, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+
+            int size = (int)stream.Length;
+            char[] contents = new char[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                contents[i] = (char)reader.Read();
+            }
+            
+            foreach(char ch in contents)
+            {
+                Console.WriteLine(ch);
+            }
+        }
+    }
+}
---
+++ task6/ITMO.CSCourse2022.Task6.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Task6._1
+{
+    public enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+    class CreateAccount
+    {
+        static BankAccount NewBankAccount()
+        {
+            BankAccount created = new BankAccount();
+            created.Populate(number, balance);
+        }
+    }
+    class BankAccount
+    {   
+        public void Populate( long number, decimal balance)
+        {
+            accNo = number;
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+    }
+    internal class ProgramStruct
+    {
+        static void Main()
+        {
+            BankAccount goldAccount;
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+
+            Console.Write("Enter account number: ");
+            goldAccount.accNo = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("*** Account Summary ***");
+            Console.WriteLine("Account Number: {0}", goldAccount.accNo);
+            Console.WriteLine("Account Type: {0}", goldAccount.accType);
+            Console.WriteLine("Account Balance: {0}", goldAccount.accBal);
+
+        }
+    }
+}
