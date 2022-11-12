7c9b468ba39de5706c688fc6b8f43d98c4d570b4
Sergey Gorbanev <gsv80@ya.com>
2022-11-13 00:29:27 +0300

    Task8.2 added

---
+++ task8/ITMO.CSCourse2022.Task8.2/BankTransaction.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+public class BankTransaction
+{
+    private readonly decimal amount;
+    private readonly DateTime when;
+
+    public decimal Amount()
+    {
+        return amount;
+    }
+
+    public DateTime When()
+    {
+        return when;
+    }
+
+    public BankTransaction(decimal tranAmount)
+    {
+        amount = tranAmount;
+        when = DateTime.Now;
+    }
+}
+
